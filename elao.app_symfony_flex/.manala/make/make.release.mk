d522c11b6135e0c37bc0141e6745d1378832dd8e
xavier-rdo <xavier.roldo@elao.com>
2019-02-12 10:34:06 +0100

    [app_symfony_flex] Some updates about release in  Symfony Flex app's template

--- elao.app_symfony_flex/.manala/make/make.release.mk
+++ elao.app_symfony_flex/.manala/make/make.release.mk
@@ -1,19 +1,24 @@
-release@production:
-	docker run \
-        --rm \
-        --tty \
-        --mount type=bind,source=$(PWD),target=/srv \
-        --mount type=bind,source=$(HOME)/.ssh/id_rsa,target=/home/app/.ssh/id_rsa \
-        --mount type=bind,source=$(HOME)/.gitconfig,target=/home/app/.gitconfig \
-        $(DOCKER_IMAGE) \
-        ansible-playbook .manala/ansible/release.yaml --inventory .manala/ansible/release.inventory.yaml --limit production
+########
+# Diff #
+########
 
-release@staging:
-	docker run \
+# Build and publish a release for a given environment target.
+#
+# @param $1 The environment target, eg production, preprod, staging
+#
+# Examples:
+#
+# Example #1: make a release for the production environment
+#
+#   $(call release,production)
+
+define release
+        docker run \
         --rm \
         --tty \
         --mount type=bind,source=$(PWD),target=/srv \
         --mount type=bind,source=$(HOME)/.ssh/id_rsa,target=/home/app/.ssh/id_rsa \
         --mount type=bind,source=$(HOME)/.gitconfig,target=/home/app/.gitconfig \
         $(DOCKER_IMAGE) \
-        ansible-playbook .manala/ansible/release.yaml --inventory .manala/ansible/release.inventory.yaml --limit staging
+        ansible-playbook .manala/ansible/release.yaml --inventory .manala/ansible/release.inventory.yaml --limit $(1)
+endef
